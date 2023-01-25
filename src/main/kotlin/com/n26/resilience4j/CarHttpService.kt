036d0be2be6b4d2518a621951e616a9b9c0b5d24
Oscar Perez <operez.o@wio.io>
2023-01-25 18:42:52 +0200

    First commit

---
+++ src/main/kotlin/com/n26/resilience4j/CarHttpService.kt
@@ -0,0 +1,4 @@
+package com.n26.resilience4j
+
+class CarHttpService {
+}
---
+++ src/main/kotlin/com/n26/resilience4j/Resilience4jApplication.kt
@@ -0,0 +1,11 @@
+package com.n26.resilience4j
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Resilience4jApplication
+
+fun main(args: Array<String>) {
+	runApplication<Resilience4jApplication>(*args)
+}
